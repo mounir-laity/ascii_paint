a1ec6e9ffd8eea7d12b2f7318285bbbf776958eb
Mounir Laity <53649@etu.he2b.be>
2020-02-08 19:16:51 +0100

    Ajout de la classe Square

---
+++ src/Model/Square.java
@@ -0,0 +1,13 @@
+package Model;
+
+/**
+ *
+ * @author laity
+ */
+public class Square extends Rectangle{
+
+    public Square(Point upperLeft, double side, char color) {
+        super(upperLeft, side, side, color);
+    }
+    
+}
