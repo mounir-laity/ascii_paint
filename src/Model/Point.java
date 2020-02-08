e13ff1af44aa9a75f83b5fd907a7dea013cbe95e
Mounir Laity <53649@etu.he2b.be>
2020-02-08 18:32:09 +0100

    Ajout de la classe point

---
+++ src/Model/Point.java
@@ -0,0 +1,38 @@
+package Model;
+
+/**
+ *
+ * @author laity
+ */
+public class Point {
+
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this(p.x, p.y);
+    }
+
+    void move(double dx, double dy) {
+        this.x = x + dx;
+        this.y = y + dy;
+    }
+
+    double distanceTo(Point other) {
+        return (Math.hypot((other.x - this.x), (other.y - this.y)));
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    
+}
