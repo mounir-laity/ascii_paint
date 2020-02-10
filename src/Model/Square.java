88e8dbf586c723141925e7b5b93013fb3aed71ca
Mounir Laity <53649@etu.he2b.be>
2020-02-10 21:11:08 +0100

    Ajout d'une condition du côté du carré > 0

--- src/Model/Square.java
+++ src/Model/Square.java
@@ -8,6 +8,9 @@ public class Square extends Rectangle {
 
     public Square(Point upperLeft, double side, char color) {
         super(upperLeft, side, side, color);
+        if(side < 0) {
+            throw new IllegalArgumentException("The side must be positive");
+        }
     }
 
 }
